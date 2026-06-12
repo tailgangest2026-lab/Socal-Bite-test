4d6b60e490cef04b1f9c6d76f6061ffaea4b7ebf
tailgangest2026-lab <tail.gang.est.2026@gmail.com>
2026-06-12 14:42:52 -0700

    Create site-config.js

---
+++ js/site-config.js
@@ -0,0 +1,8 @@
+forecast.js:31  GET https://tailgangest2026-lab.github.io/home.json?v=2026-06-05 404 (Not Found)
+fetchJson @ forecast.js:31
+initForecast @ forecast.js:10Understand this error
+forecast.js:22 Forecast load failed: Error: Could not load ../home.json
+    at fetchJson (forecast.js:34:11)
+    at async HTMLDocument.initForecast (forecast.js:10:20)
+initForecast @ forecast.js:22Understand this error
+favicon.ico:1  GET https://tailgangest2026-lab.github.io/favicon.ico 404 (Not Found)
